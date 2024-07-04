6073dd2a0ead51c389811b577676c25b1ee4cc24
David Balatero <dbalatero@users.noreply.github.com>
2024-07-04 17:09:19 -0400

    bump sorbet

--- lib/interview.rb
+++ lib/interview.rb
@@ -1,5 +1,12 @@
+# typed: strong
+
 module Interview
   class << self
+    extend T::Sig
+
+    sig do
+      params(a: Numeric, b: Numeric).returns(Numeric)
+    end
     def foo(a, b)
       a + b
     end
