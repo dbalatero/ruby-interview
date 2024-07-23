b3a1063666c9c6622a74a8123d642101e0f06ceb
David Balatero <dbalatero@users.noreply.github.com>
2024-07-23 08:57:32 -0400

    remove

--- lib/interview.rb
+++ lib/interview.rb
@@ -4,12 +4,5 @@
 module Interview
   class << self
     extend T::Sig
-
-    sig do
-      params(a: Numeric, b: Numeric).returns(Numeric)
-    end
-    def foo(a, b)
-      a + b
-    end
   end
 end
